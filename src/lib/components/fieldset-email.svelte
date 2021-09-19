da72a95c6b891ed60e7bb318ea9c095e00cf35b7
emrah <emrah.com@gmail.com>
2021-09-19 22:52:37 +0300

    feat: email input component

---
+++ src/lib/components/fieldset-email.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import Messages from "$lib/components/messages.svelte";
+import type { Node } from "$lib/kratos";
+
+export let node: Node;
+
+const attr = node.attributes;
+let labelText: string;
+
+try {
+  labelText = node.meta.label.text;
+} catch {
+  labelText = attr.name;
+}
+</script>
+
+<fieldset>
+  <label>
+    <span>{labelText}</span>
+    <input type="email" name="{attr.name}" value="{attr.value || ""}"
+      placeholder="{labelText}" disabled={attr.disabled}
+      required={attr.required}>
+  </label>
+
+  <Messages messages={node.messages} />
+</fieldset>
--- src/lib/components/fieldsets.svelte
+++ src/lib/components/fieldsets.svelte
@@ -2,6 +2,7 @@
 import Hidden from "$lib/components/fieldset-hidden.svelte";
 import Password from "$lib/components/fieldset-password.svelte";
 import Text from "$lib/components/fieldset-text.svelte";
+import Email from "$lib/components/fieldset-email.svelte";
 import Submit from "$lib/components/fieldset-submit.svelte";
 import type { Node } from "$lib/kratos";
 
@@ -15,6 +16,8 @@ export let nodes: Node[];
     <Password {node} />
   {:else if node.attributes.type === "text"}
     <Text {node} />
+  {:else if node.attributes.type === "email"}
+    <Email {node} />
   {:else if node.attributes.type === "submit"}
     <Submit {node} />
   {:else}
