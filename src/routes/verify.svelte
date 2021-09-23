7ae9b686bf156ab4c30626ce301d0aff417f5aad
emrah <emrah.com@gmail.com>
2021-09-23 13:15:48 +0300

    feat: verification

---
+++ src/routes/verify.svelte
@@ -0,0 +1,30 @@
+<script lang="ts" context="module">
+  import { loadDataModels } from "$lib/kratos";
+  import type { LoadOutput } from "$lib/custom-types";
+
+  export async function load(): Promise<LoadOutput> {
+    return await loadDataModels("verification");
+  }
+</script>
+
+<!-- -------------------------------------------------------------------------->
+<script lang="ts">
+  import type { KratosForm, KratosError } from "$lib/kratos-types";
+  import Form from "$lib/components/form.svelte";
+
+  export let dm: KratosForm | KratosError;
+
+  if (dm.instanceOf === "KratosError") console.error(dm);
+</script>
+
+<!-- -------------------------------------------------------------------------->
+{#if dm.instanceOf === "KratosForm"}
+  <div class="container" id="verification">
+    <h2 class="subheading">verification</h2>
+
+    <Form {dm} groups={["default", "link"]} />
+
+  </div>
+{:else}
+  <p>Something went wrong</p>
+{/if}
