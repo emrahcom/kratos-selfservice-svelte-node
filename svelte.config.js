f767ddb42f86ceaefdf0ef20a6138860687e9dd1
emrah <emrah.com@gmail.com>
2022-01-21 21:33:50 +0300

    refactor: update svelte configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import preprocess from "svelte-preprocess";
+//import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,19 +9,14 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
+    adapter: adapter(),
+
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: adapter({
-      // default options are shown
-      pages: "build",
-      assets: "build",
-      fallback: null,
-    }),
+
     files: {
       lib: "src/lib",
     },
-    hydrate: true,
-    ssr: false,
   },
 };
 
