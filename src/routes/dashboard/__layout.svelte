3ca01de7dab9241e0ecc135c0fcf8e12113f1956
emrah <emrah.com@gmail.com>
2022-07-24 20:59:44 +0300

    fix: redirect in dashboard

--- src/routes/dashboard/__layout.svelte
+++ src/routes/dashboard/__layout.svelte
@@ -3,10 +3,11 @@
   import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
 
+  // ---------------------------------------------------------------------------
   export async function load() {
     const _identity = get(identity);
 
-    if (!_identity) {
+    if (!_identity.id) {
       return {
         status: 302,
         redirect: `${KRATOS}/self-service/login/browser`,
--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -4,19 +4,13 @@
   import identity from "$lib/stores/kratos/identity";
 
   const _identity = get(identity);
-
-  if (!_identity) window.location.replace(`${APP}/login`);
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <section id="dashboard">
-  {#if !_identity.id}
-    <p>redirecting...</p>
-  {:else}
-    <h2>Dashboard</h2>
+  <h2>Dashboard</h2>
 
-    <p>Hello {$identity.traits.email}</p>
-    <p><a href="{APP}/settings">Settings</a></p>
-    <p><a href="{APP}/logout">Logout</a></p>
-  {/if}
+  <p>Hello {$identity.traits.email}</p>
+  <p><a href="{APP}/settings">Settings</a></p>
+  <p><a href="{APP}/logout">Logout</a></p>
 </section>
