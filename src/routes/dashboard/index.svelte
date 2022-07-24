f6fa6acb5cc4b2962e8fef0a4485555830e04e2f
emrah <emrah.com@gmail.com>
2022-07-24 20:52:45 +0300

    refactor: redirect to the login if not authenticated

--- src/routes/dashboard/index.svelte
+++ src/routes/dashboard/index.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import { APP } from "$lib/config";
+  import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
+
+  const _identity = get(identity);
+
+  if (!_identity) window.location.replace(`${APP}/login`);
 </script>
 
 <!-- -------------------------------------------------------------------------->
-<h2>Dashboard</h2>
-<p>Hello {$identity.traits.email}</p>
-<a href="{APP}/settings">Settings</a>
-<a href="{APP}/logout">Logout</a>
+<section id="dashboard">
+  {#if !_identity.id}
+    <p>redirecting...</p>
+  {:else}
+    <h2>Dashboard</h2>
+
+    <p>Hello {$identity.traits.email}</p>
+    <p><a href="{APP}/settings">Settings</a></p>
+    <p><a href="{APP}/logout">Logout</a></p>
+  {/if}
+</section>
