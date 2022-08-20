5ea08c7011d2af68c135fb5d81b5bdb70466a93a
emrah <emrah.com@gmail.com>
2022-08-20 17:58:06 +0300

    fix: logout

---
+++ src/routes/logout/+page.ts
@@ -0,0 +1,15 @@
+import { browser } from "$app/env";
+import { getLogoutDataModels } from "$lib/kratos";
+
+// -----------------------------------------------------------------------------
+export async function load() {
+  if (!browser) return {};
+
+  const dm = await getLogoutDataModels();
+
+  if (dm.instanceOf === "KratosLogout") {
+    window.location.replace(dm.logout_url);
+  } else {
+    window.location.replace("/");
+  }
+}
