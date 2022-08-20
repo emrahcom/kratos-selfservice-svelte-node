e229150f9b4040fa66bbb6a3fdac50b75f7735ec
emrah <emrah.com@gmail.com>
2022-08-20 17:18:32 +0300

    refactor: svelte configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  server: {
+    hmr: {
+      clientPort: 3000,
+    },
+  },
 };
 
 export default config;
