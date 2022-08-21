18cce9677f81fe4d3fd58a460a849a7e8d8e978d
emrah <emrah.com@gmail.com>
2022-08-21 13:11:48 +0300

    fix: email in dashboard

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -4,11 +4,11 @@
   import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
 
-  if (browser) {
-    const _identity = get(identity);
-    const loginUrl = `${KRATOS}/self-service/login/browser`;
+  const _identity = get(identity);
 
-    if (!_identity.id) window.location.replace(loginUrl);
+  if (browser && !_identity.id) {
+    const loginUrl = `${KRATOS}/self-service/login/browser`;
+    window.location.replace(loginUrl);
   }
 </script>
 
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import { APP } from "$lib/config";
+  import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
+
+  const _identity = get(identity);
+  let email = "";
+
+  if (_identity && _identity.traits) email = _identity.traits.email;
 </script>
 
 <!-- -------------------------------------------------------------------------->
 <section id="dashboard">
   <h2>Dashboard</h2>
 
-  <p>Hello {$identity.traits.email}</p>
+  <p>Hello {email}</p>
   <p><a href="{APP}/settings">Settings</a></p>
   <p><a href="{APP}/logout">Logout</a></p>
 </section>
