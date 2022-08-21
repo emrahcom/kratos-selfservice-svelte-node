6f938a71a77c6a206af4fdd033007b9aaaa517a7
emrah <emrah.com@gmail.com>
2022-08-21 13:28:30 +0300

    refactor: show identity in console

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,12 @@
   import { get } from "svelte/store";
   import identity from "$lib/stores/kratos/identity";
 
-  if (browser) {
-    const _identity = get(identity);
+  const _identity = get(identity);
 
+  if (browser) {
     if (_identity.id) {
       console.log("signed in");
+      console.log(_identity);
     } else {
       console.log("not signed in");
     }
