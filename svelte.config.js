660af48bcafe543fd2439b17f7e257a3acee6a7d
emrah <emrah.eryilmaz@nordeck.net>
2022-12-25 16:37:09 +0300

    refactor: update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 //import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-node";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
